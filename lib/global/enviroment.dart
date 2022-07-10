add497f2295bd208d288c3a5567f33d2effcd154
car2854 <car2854@gmail.com>
2022-07-10 16:03:44 -0400

    .

--- lib/global/enviroment.dart
+++ lib/global/enviroment.dart
@@ -1,5 +1,5 @@
 
 class Enviroment {
-  static String apiUrl = 'http://192.168.0.4:4000/api';
-  // static String apiUrl = 'https://api-eticket.herokuapp.com/api';
+  // static String apiUrl = 'http://192.168.0.4:4000/api';
+  static String apiUrl = 'https://api-eticket.herokuapp.com/api';
 }
