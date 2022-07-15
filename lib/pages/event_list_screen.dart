135df588610f619510c3d9b02c23e6ef7655e84a
car2854 <car2854@gmail.com>
2022-07-15 18:30:04 -0400

    diseñar algo sobre los eventos

--- lib/pages/event_list_screen.dart
+++ lib/pages/event_list_screen.dart
@@ -26,37 +26,116 @@ class EventListScreen extends StatelessWidget {
 
                   if (snapshot.data!){
                     return Expanded(
-                      child: ListView.builder(
-                        itemCount: eventBloc.eventModel!.length,
-                        physics: const BouncingScrollPhysics(),
-                        // padding: EdgeInsets.all(20),
-                        itemBuilder: (BuildContext context, int index) { 
-                          return GestureDetector(
-                            onTap: (){
-                              // TODO: Click card
-                              eventBloc.add(OnSelectEvent(eventBloc.eventModel![index]));
-                              Navigator.pushNamed(context, 'scan_event');
-                            },
-                            child: Card(
-                              child: Column(  
-                                mainAxisSize: MainAxisSize.min,  
-                                children: <Widget>[  
-                                  ListTile(
-                                    title: Text(eventBloc.eventModel![index].evento),  
-                                    subtitle: Text(eventBloc.eventModel![index].ubicacion), 
-                                    trailing: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Text(eventBloc.eventModel![index].fecha),
-                                        Text(eventBloc.eventModel![index].hora)
-                                      ],
-                                    ),
-                                  ),  
-                                ],  
-                              ),  
+                      child: Container(
+                        color: const Color.fromARGB(255, 251, 251, 251),
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 15, top: 5),
+                              child: const Text('Eventos asignados')
                             ),
-                          );
-                         },
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: eventBloc.eventModel!.length,
+                                physics: const BouncingScrollPhysics(),
+                                // padding: EdgeInsets.all(20),
+                                itemBuilder: (BuildContext context, int index) { 
+                                  return GestureDetector(
+                                    onTap: (){
+                                      // TODO: Click card
+                                      eventBloc.add(OnSelectEvent(eventBloc.eventModel![index]));
+                                      Navigator.pushNamed(context, 'scan_event');
+                                    },
+                                    child: Card(
+                                      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                      child: Column(  
+                                        mainAxisSize: MainAxisSize.min,  
+                                        children: <Widget>[
+                                                
+                                            Column(
+                                              children: [
+                                                Container(
+                                                  width: double.infinity,
+                                                  height: 50.0,
+                                                  padding: const EdgeInsets.only(left: 25),
+                                                  decoration: const BoxDecoration(
+                                                    color: Color(0xD02196F3)
+                                                  ),
+                                                  child: Align(
+                                                    alignment: Alignment.centerLeft,
+                                                    child: Text(
+                                                      eventBloc.eventModel![index].evento, 
+                                                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                                                    )
+                                                  ),
+                                                ),
+                                                Column(
+                                                  children: [
+                                                    Container(
+                                                      margin: const EdgeInsets.only(left: 20, top: 15),
+                                                      child: Align(
+                                                        alignment: Alignment.centerLeft,
+                                                        child: Text(
+                                                          eventBloc.eventModel![index].ubicacion,
+                                                          style: const TextStyle(fontSize: 14),
+                                                        )
+                                                      ),
+                                                    ),
+                                                    const SizedBox(height: 18,),
+                                                    Container(
+                                                      margin: const EdgeInsets.only(bottom: 10),
+                                                      child: Row(
+                                                        mainAxisAlignment: MainAxisAlignment.end,
+                                                        children: [
+                                                          Container(
+                                                            margin: const EdgeInsets.only(right: 20),
+                                                            child: Row(
+                                                              children: [
+                                                                const Text('Fecha: ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),),
+                                                                Text(eventBloc.eventModel![index].fecha, style: const TextStyle(fontSize: 13),),
+                                                              ],
+                                                            ),
+                                                          ),
+                                                          Container(
+                                                            margin: const EdgeInsets.only(right: 20),
+                                                            child: Row(
+                                                              children: [
+                                                                const Text('Hora: ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),),
+                                                                Text(eventBloc.eventModel![index].hora, style: const TextStyle(fontSize: 13),),
+                                                              ],
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    )
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                                
+                                            
+                                                
+                                          // ListTile(
+                                          //   title: Text(eventBloc.eventModel![index].evento),  
+                                          //   subtitle: Text(eventBloc.eventModel![index].ubicacion), 
+                                          //   trailing: Column(
+                                          //     mainAxisAlignment: MainAxisAlignment.center,
+                                          //     children: [
+                                          //       Text(eventBloc.eventModel![index].fecha),
+                                          //       Text(eventBloc.eventModel![index].hora)
+                                          //     ],
+                                          //   ),
+                                          // ),  
+                                        ],  
+                                      ),  
+                                    ),
+                                  );
+                                 },
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   }else{
